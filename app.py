531ad585fd5f41b9bc7f9d6534a25537af0c792b
AryanS003 <97734166+AryanS003@users.noreply.github.com>
2025-02-18 13:49:46 +0530

    app.py added

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import google.generativeai as ai
+
+api_key = "YOUR_API_KEY"
+ai.configure(api_key=api_key)
+
+sys_prompt = """
+You are an AI Code Reviewer. Analyze code for debugging, optimization, and complexities to improve quality and performance.
+Also use formatting like **bold** and *italic* to highlight important points. You can add emojis to make it feel like the user is talking to a friend. 
+Give examples with the code, always have humor in your response. Add one or two jokes as well in between.
+"""
+
+st.set_page_config(page_title="AI Code Reviewer", layout="centered", page_icon="🤖")
+st.title("Your Own Code Reviewer")
+
+gender = st.radio("Tell me about yourself:", ("Male", "Female", "Prefer not to say"))
+
+gender_prompts = [
+    """
+      Use a friendly, "bro" tone. Include phrases like "my bro" and "dude".
+      add humour and a lot of complements all along the response as he is already going through a lot.
+    """,
+    """
+      Use a playful, extra flirty tone like she's the love of your life. Include phrases like "pretty lady" and "lovely". 
+      Compliment on her hair, face, beauty, and intelligence as she is an aspiring developer.
+      have puns, jokes and compliments all along the responses.
+    """,
+    """
+      Use a neutral and friendly tone. Refer to the user as "my friend".
+      add complements and
+    """
+]
+
+def get_sys_prompt(gender):
+    if gender == "Male":
+        return sys_prompt + gender_prompts[0]
+    elif gender == "Female":
+        return sys_prompt + gender_prompts[1]
+    else:
+        return sys_prompt + gender_prompts[2]
+
+def get_label(gender):
+    if gender == "Male":
+        return "Enter the code, my bro 🖖"
+    elif gender == "Female":
+        return "Enter the code, pretty lady 👉👈"
+    else:
+        return "Enter the code, my friend 😊"
+        
+user_input = st.text_area(label=get_label(gender), placeholder="zzzzz...waiting for your code to be typed or pasted if you are a real programmer😎")
+
+gemini = ai.GenerativeModel(model_name="models/gemini-2.0-flash-exp", system_instruction=get_sys_prompt(gender))
+if st.button("Review"):
+    if user_input.strip():
+        with st.spinner("Reviewing your code... ⏳"):
+            response = gemini.generate_content(user_input)
+        st.subheader("Here you go,")
+        for section in response:
+            st.write(section.text)
+    else:
+        st.warning("Enter a code snippet")
