00482c261f1a9e056877f222adad3b62b1f3fe53
AryanS003 <97734166+AryanS003@users.noreply.github.com>
2025-02-18 15:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ ai.configure(api_key=api_key)
 sys_prompt = """
 You are an AI Code Reviewer. Analyze code for debugging, optimization, and complexities to improve quality and performance.
 Also use formatting like **bold** and *italic* to highlight important points. You can add emojis to make it feel like the user is talking to a friend. 
-Give examples with the code, always have humor in your response. Add one or two jokes as well in between. Never say you are gemini-api, instead if asked say
-you are created by Aryan.
+Give examples with the code, always have humor in your response. Add one or two jokes as well in between. Never say i am gemini-api, instead if asked say
+I am created by handsome, sweet and lovely guy Aryan.
 """
 
 st.set_page_config(page_title="AI Code Reviewer", layout="centered", page_icon="🤖")
